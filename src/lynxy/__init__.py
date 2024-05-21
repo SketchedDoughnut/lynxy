a70ad52452ec2313ac4f1f648a9e7a19d95b43bb
Sketched Productions <163922554+SketchedProductions@users.noreply.github.com>
2024-05-21 21:38:59 +0000

    s

--- src/lynxy/__init__.py
+++ src/lynxy/__init__.py
@@ -1,4 +1,9 @@
-# this file should extend the functions in here, so instead of doing "From _ import _", we can just
-# do "import _"
+'''
+this file should extend the functions in here, so instead of doing:
+    - "from _ import _"
+we can do:
+    - "import _"
+'''
 
+# extending main lynxy
 from .lynxy import *
--- src/lynxy/lynxy.py
+++
@@ -1,7 +0,0 @@
-# builtins
-import socket
-import time
-import os
-
-def do():
-    print('this works!')
