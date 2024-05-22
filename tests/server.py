bcd74f7d43fc949120ddb11e732b61819abef073
Sketched Productions <163922554+SketchedProductions@users.noreply.github.com>
2024-05-22 21:28:15 +0000

    save

--- tests/server.py
+++ tests/server.py
@@ -26,48 +26,30 @@ client_dict = {
 }
 
 unique_server_instance_dict = {}
-# active_dict = {}
-# for i in range(INSTANCE_LIMIT):
-#     active_dict[i] = False
-
-# def start_info_handler() -> None:
-#     info_port = 22222
-#     while True:
-#         info_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         print('info server made')
-#         info_server.bind(('', info_port))
-#         print('info server binded to port', info_port)
-#         info_server.listen(5)
-#         print('info server listening')
-#         info_client, info_client_address = info_server.accept()
-#         print('info server accepted from:', info_client_address)
-#         msg = f'{active_dict}'.encode('utf-8')
-#         info_client.send(msg)
-#         print('message sent')
-#         info_client.close()
-#         info_server.close()
-#         print('info client, info_server closed')
+# active_instance_dict = {}
 
 # connect to client
-def connect_to_client(server: socket.socket) -> list[socket.socket, str]:
+def connect_to_client(server: socket.socket, return_port: bool = False) -> list[socket.socket, str, int]:
     global port
+    local_port = port
     # bind to first incoming ip, and port
     #server.bind(('', port))
     #print(f'server binded to port {port}')
     while True:
         try:
-            print('attempting to bind to port:', port)
-            server.bind(('', port))
-            print(f'server binded to port {port}')
+            print('attempting to bind to port:', local_port)
+            server.bind(('', local_port))
+            print(f'server binded to port {local_port}')
             break
         except:
-            if valid_ports.index(port) < not_inclusive_max_port_amount:
+            if valid_ports.index(local_port) < not_inclusive_max_port_amount:
                 print('port bind failed, cycling')
-                port = valid_ports[valid_ports.index(port) + 1]
-                print('swapped to port', port)
+                local_port = valid_ports[valid_ports.index(local_port) + 1]
+                print('swapped to port', local_port)
             else:
                 print('all ports are being used, cancelling attempt')
                 exit()
+    port = local_port
 
     # listen for an incoming connection
     print('server listening for a connection...')
@@ -84,7 +66,10 @@ def connect_to_client(server: socket.socket) -> list[socket.socket, str]:
         msg = "verify_confirm".encode('utf-8')
         client.send(msg)
         print('message sent')
-        return [client, addr]
+        if return_port == True:
+            return [client, addr, local_port]
+        else:
+            return [client, addr]
 
 # intake username data
 # def recieve_username_data(client: socket.socket, msg: str) -> str:
@@ -97,16 +82,37 @@ def connect_to_client(server: socket.socket) -> list[socket.socket, str]:
 #             print(f'username recieved: {username}')
 #             return username
 
-def check_alive(client: socket.socket) -> bool:
+
+
+
+
+
+
+
+
+
+def check_alive() -> None:
     global unique_server_instance_dict
     import time
-    time.sleep(5)
-    state = is_socket_closed(client)
-    if state == True:
-        print('socket has been detected as dead, changing state to dead (false)')
-        unique_server_instance_dict[client] = False
-        client.close()
-    
+    while True:
+        time.sleep(5)
+        for por in valid_ports:
+            data = unique_server_instance_dict[port]
+            state = data[0]
+            server_obj = data[1]
+            client_obj = data[2]
+            state = is_socket_closed(client_obj)
+            if state == True:
+                print(f'the client for port {por} is not responsive, so the connection will be terminated.')
+                unique_server_instance_dict[por] = [False, server_obj, client_obj]
+        
+
+
+
+
+
+
+
 # https://stackoverflow.com/questions/48024720/python-how-to-check-if-socket-is-still-connected
 def is_socket_closed(sock: socket.socket) -> bool:
     try:
@@ -123,11 +129,39 @@ def is_socket_closed(sock: socket.socket) -> bool:
         return False
     return False
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # log username data into client_dict
 def log_username_data(username: str, address: str, c_dict: dict) -> dict:
     c_dict[username] = address
     print('logged username and address to client dict')
     return c_dict
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def answer_request_by_username(client: socket.socket, c_dict: dict, cmd: str) -> None:
     # cmd = client.recv(1024).decode('utf-8')
@@ -164,30 +198,20 @@ def answer_request_by_username(client: socket.socket, c_dict: dict, cmd: str) ->
 
 
 # MAIN MESSAGE HANDLER
-def input_handler(client: socket.socket, client_address: str) -> None:
+def input_handler(client: socket.socket, client_address: str, server: socket.socket, server_port: int) -> None:
     global client_dict, alive_bound_instance
     print('----------------------')
     print('INPUT HANDLER STARTED')
     print('----------------------')
-    # print('starting ping thread...')
-    # threading.Thread(target=lambda:check_alive(client), daemon=True).start()
+    print('starting alive check loop...')
+    threading.Thread(target=lambda:check_alive()).start()
     while True: 
-        # passed = False
-        # try:
-        #     msg = client.recv(1024).decode('utf-8')
-        #     passed = True
-        # except:
-        #     print('error listening from client, re-connecting to any ip...')
-        #     main_client, client_address = connect_to_client(main_server)
-        # if passed == True:
-        # if unique_server_instance_dict[client] == False:
-        #     print('client has been deemed dead, killing')
-        #     client.close()
-        #     alive_bound_instance -= 1
-        #     print('bound count changed to', alive_bound_instance)
-        #     print('searching count is', alive_searching_instance)
-        #     print('current unique dict:', unique_server_instance_dict)
-        #     exit()
+        if unique_server_instance_dict[server_port][0] == False:
+            print(f'this server on port {server_port} has no active client, and will therefore terminate its process')
+            server.close()
+            client.close()
+            alive_bound_instance -= 1
+            exit()
         msg = client.recv(1024).decode('utf-8')
         if msg:
             split_msg = msg.split()
@@ -212,15 +236,18 @@ def input_handler(client: socket.socket, client_address: str) -> None:
 def start_server_instance(server: socket.socket, instance_at_start: int) -> None:
     print(f'starting a new searching instance with num: {instance_at_start}')
     global alive_searching_instance, alive_bound_instance, unique_server_instance_dict
-    main_client, client_address = connect_to_client(server)
+    main_client, client_address, server_port = connect_to_client(server, return_port=True)
     print('binded to client, decreasing search and increasing bound')
     alive_searching_instance -= 1
     alive_bound_instance += 1
     print('bound is now:', alive_bound_instance)
-    print('logging client address to detect if closed later')
-    unique_server_instance_dict[main_client] = True
+    print('signing in to active dict with port, logging True and main_client')
+    # active_instance_dict[server_port] = [True, main_client]
+    unique_server_instance_dict[server_port] = [True, server, main_client]
+    # print('logging client address to detect if closed later')
+    # unique_server_instance_dict[main_client] = True
     print('transferring to input handler')
-    input_handler(main_client, client_address)
+    input_handler(main_client, client_address, server, server_port)
 
 
 
@@ -248,8 +275,11 @@ def start_instance_handler() -> None:
 
 
 
-# start info handler
-# start_info_handler()
+
+
+
+
+
 
 # start normal handler
 start_instance_handler()
