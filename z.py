4817d8985e0bbb2d0d761aeb3e4cff4c5cd4339e
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-26 16:57:38 -0700

    s

--- z.py
+++ z.py
@@ -9,7 +9,7 @@ from src import lynxy_server as ls
 ip, port, auth_token = ls.start_server()
 
 # client
-# l.disable_print()
+l.disable_print()
 l.start_client(input('-> '))
 l.general_send(f'{auth_token}')
 l.general_send('freeze_server')
@@ -20,4 +20,4 @@ print(l.request_username_data('SketchedDoughnut')) # will work
 
 
 print(ls.freeze_server())
-l.general_send('username S')
+l.general_send('username test')
