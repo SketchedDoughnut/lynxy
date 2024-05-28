07c36caf166c52850c715eefdb83f45b396c8053
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-28 10:19:40 -0700

    Update __init__.py

--- src/lynxy/__init__.py
+++ src/lynxy/__init__.py
@@ -1,6 +1,6 @@
 '''
 This is the lynxy client module. To find documentation, go to the github~!
-- Github: https://github.com/SketchedDoughnut/LYNX
+- Github: https://github.com/SketchedDoughnut/lynxy
 '''
 
 # extending main lynx file
