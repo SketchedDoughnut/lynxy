644c0ec043137ef9f04ca25f7f1b724c3119a294
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-27 21:18:53 -0700

    s

--- z.py
+++ z.py
@@ -30,9 +30,9 @@ from src import lynxy as l
 ip = input('-> ')
 l.enable_print()
 l.start_client(ip)
-l.submit_username_data('SketchedDoughnut')
+l.submit_username_data('1')
 input('-> ')
-data = l.request_username_data('laptop')
+data = l.request_username_data('2')
 i = data[0]
 p = data[1]
 print(i)
