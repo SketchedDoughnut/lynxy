1d7f4f1b8d44e55f4c5f681025bac1a078249e4b
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-27 20:14:47 -0700

    what

--- z.py
+++ z.py
@@ -1,25 +1,12 @@
-# import lynxy
+import lynxy
 
-# lynxy.enable_print()
-# ip = input('-> ')
-# lynxy.start_client(ip)
-# while True:
-#     msg = input('-> ')
-#     # msg = 't'
-#     if msg == 'break':
-#         break
-#     lynxy.send_msg(msg, recieve=True)
-# lynxy.shutdown_client()
-
-# import lynxy_server as ls
-from src import lynxy_server as ls
-
-ip, port, token = ls.start_server()
-
-from src import lynxy as l
-l.enable_print()
-l.start_client(ip)
-l.send_msg(" ", recieve=True)
-l.send_msg('username', recieve=True)
-l.send_msg('username SketchedDoughnut', recieve=True)
-l.send_msg('clear_client', recieve=True)
+lynxy.enable_print()
+ip = input('-> ')
+lynxy.start_client(ip)
+while True:
+    msg = input('-> ')
+    # msg = 't'
+    if msg == 'break':
+        break
+    lynxy.send_msg(msg, recieve=True)
+lynxy.shutdown_client()
