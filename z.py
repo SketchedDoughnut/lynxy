82c868eca2d8add3c89b147c7f3cc10d1b41f73e
Sketched Productions <163922554+SketchedProductions@users.noreply.github.com>
2024-05-30 20:14:42 +0000

    messing around with example

--- z.py
+++ z.py
@@ -1,12 +1,22 @@
-import lynxy
+import lynxy as l
+import lynxy_server as ls
 
-lynxy.enable_print()
-ip = input('-> ')
-print('state of connect:', lynxy.start_client(ip))
+def run_server():
+    ip, port, token = ls.start_server()
+    return (ip, port, token)
+
+
+# running
+ip, port, token = run_server()
+
+l.enable_print()
+# ip = input('-> ')
+ip = ip
+print('state of connect:', l.start_client(ip))
 while True:
     msg = input('-> ')
     # msg = 't'
     if msg == 'break':
         break
-    lynxy.send_msg(msg, recieve=True)
-lynxy.shutdown_client()
+    l.send_msg(msg, recieve=True)
+l.shutdown_client()
