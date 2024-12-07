bbcb644c0c76315b9ac45c4f6c28b7dc887113f8
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-12-07 03:58:40 +0000

    comitting to save

--- src/lynxy/constants.py
+++ src/lynxy/constants.py
@@ -1,6 +1,13 @@
-# this file contains all constants
+'''
+PUT INTRODUCTORY HEADER HERE, INCLUDE ANY OTHER INFORMATION
+'''
+
+# included modules
 from enum import Enum
 
+# the main class for containing all of the Enums
+# basically fancy constants that keep their color
 class Constants(Enum):
     DO_PRINT = 'print'
     DEFAULT_PORTS = 'port_override'
+    PLACEHOLDER = 'placeholder'
