dc604925b4319e2d3789a8bfd95b2242fff2d17b
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-12-08 01:00:55 -0800

    s

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 from src import lynxy
 
-inst = lynxy.Lynxy(host_port=11111, bind=True)
-print('initialized...')
-print(inst.comm.get_host())
-connect_ip = input('-> ')
-connect_port = int(input('-> '))
-print('connecting...')
-inst.connect(connect_ip, connect_port)
-inst.close()
+def c1():
+    inst = lynxy.Lynxy(host_port=11111, bind=True)
+    print('initialized...')
+    print(inst.comm.get_host())
+    connect_ip = '192.168.68.124'
+    connect_port = 11112
+    print('connecting...')
+    inst.connect(connect_ip, connect_port)
+    inst.close()
+
+def c2():
+    inst = lynxy.Lynxy(host_port=11112, bind=True)
+    print('initialized...')
+    print(inst.comm.get_host())
+    connect_ip = '192.168.68.123'
+    connect_port = 11111
+    print('connecting...')
+    inst.connect(connect_ip, connect_port)
+    inst.close()
+
+c1()
+c2()
