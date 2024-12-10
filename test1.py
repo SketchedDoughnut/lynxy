cf5ea0f68e546f85484bcd611913c4cdda115b12
Sketched Doughnut <sketchedbusinessemail@gmail.com>
2024-12-09 19:13:10 -0800

    updating ip on 1

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ def c1():
     inst = lynxy.Lynxy(host_port=11111, bind=True)
     print('initialized...')
     print(inst.comm.get_host())
-    connect_ip = '192.168.68.124'
+    connect_ip = '192.168.68.114'
     connect_port = 11112
     print('connecting...')
     inst.connect(connect_ip, connect_port)
