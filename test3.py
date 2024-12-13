29de184cf30ebf9e24700bc628d9154948ad62ec
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-12-13 18:07:09 +0000

    solution, using literal_eval form ast

--- test3.py
+++ test3.py
@@ -1,2 +1,17 @@
 from src import lynxy
 from rich import print
+from ast import literal_eval
+
+inst = lynxy.Lynxy()
+message = 'hi'
+encrypted = inst.comm.sec.RSA_encrypt(message, True)
+padded = inst.comm.parser.addPadding(encrypted)
+unpadded = inst.comm.parser.removePadding(padded)
+elem = unpadded[0]
+# print(type(elem))
+# print(elem)
+new = literal_eval(elem)
+# print(type(new))
+# print(new)
+decrypted = inst.comm.sec.RSA_decrypt(new)
+print(decrypted)
