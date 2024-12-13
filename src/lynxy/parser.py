2010a5769979838f247651b760de28f40db89062
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-12-13 18:10:15 +0000

    implemented working change into removePadding in parser

--- src/lynxy/parser.py
+++ src/lynxy/parser.py
@@ -2,6 +2,9 @@
 PUT INTRODUCTORY HEADER HERE, INCLUDE ANY OTHER INFORMATION
 '''
 
+# included modules
+from ast import literal_eval
+
 ####################################################
 
 # this is the main class for the parser 
@@ -20,7 +23,13 @@ class Parser:
     # this function splits the messages by the start marker
     # and can optionally discard invalid endings that aren't complete
     def removePadding(self, message: str, remove_invalid: bool = True) -> list:
+        # split into list, remove empty entries
+        # only if remove_invalid is True
         splitMessage = message.split(self.endMarker)
         if len(splitMessage[-1]) == 0: splitMessage.pop(-1)
         if remove_invalid and not message.endswith(self.endMarker): splitMessage.pop(-1)
-        return splitMessage
+        # go through and convert each message in the list
+        # into bytes instead of strings
+        finalList = []
+        for elem in splitMessage: finalList.append(literal_eval(elem))
+        return finalList
