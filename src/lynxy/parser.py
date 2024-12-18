d234d8b7e2d3dfa056b92a208f0be876e88a9715
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-12-17 22:00:49 -0800

    removing uneeded prints

--- src/lynxy/parser.py
+++ src/lynxy/parser.py
@@ -32,12 +32,9 @@ class Parser:
         stitched = self.carry + message
         # split message by end marker
         split = stitched.split(self.byteEndMarker)
-        # print('split:', split)
         # if the end characters is the end marker, then that means
         # we only have complete messages so we can reset carry
-        if stitched.endswith(self.byteEndMarker):
-            # print('ends with marker, clearing carry')
-            self.carry = b''
+        if stitched.endswith(self.byteEndMarker): self.carry = b''
         # otherwise, we analyze further
         else:
             # if the length of the list is 0, then we have nothing
@@ -46,23 +43,18 @@ class Parser:
             if len(split) <= 1:
                 # save the message to carry and return
                 # empty list
-                # print('length is 1, saving to carry')
                 self.carry = stitched
                 return []
             # else, if the last entry of the list is not empty,
             # that means there is an incomplete packet there
             elif split[-1]:
                 # take the last packet which is incomplete and save to carry
-                # print('content found in last entry, saving to carry')
                 self.carry = split.pop(-1)
         # if requested, go ahead and remove all white spaces
         if remove_empty:
             index = 0
             for elem in split:
-                if not elem: 
-                    # print('removing:', index)
-                    split.pop(index)
+                if not elem: split.pop(index)
                 index += 1
         # return our final formatted list of messages
-        # print('returning:', split)
         return split
