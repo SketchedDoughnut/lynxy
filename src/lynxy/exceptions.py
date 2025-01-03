c8d6f2b63104bcd843573d8a7218aa6c6d2a8f3c
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-01-02 18:22:17 -0800

    need to do stuff for comm and lynx

--- src/lynxy/exceptions.py
+++ src/lynxy/exceptions.py
@@ -16,11 +16,6 @@ class Exceptions:
             self.message = message
             super().__init__(message)
 
-    class InvalidToggleValueError(Exception):
-        def __init__(self, message='The ID and value toggle pair is invalid.'):
-            self.message = message
-            super().__init__(message)
-
     
     class EmptyDataError(Exception):
         def __init__(self, message='The data you attempted to send is empty.'):
@@ -28,25 +23,7 @@ class Exceptions:
             super().__init__(message)
 
 
-    class TerminationSuccessError(Exception):
-        def __init__(self, message='Connection was terminated successfully!'):
-            self.message = message
-            super().__init__(message)
-
-         
-    class InvalidPortError(Exception):
-        def __init__(self, message=None):
-            self.message = message
-            super().__init__(message)
-
-
     class ConnectionFailedError(Exception):
-        def __init__(self, message=None):
-            self.message = message
-            super().__init__(message)
-
-    
-    class NoEventError(Exception):
         def __init__(self, message=None):
             self.message = message
             super().__init__(message)
