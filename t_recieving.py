65327292e79dc80657b4e72829b079b2c98e444a
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-01-02 19:17:00 -0800

    a

--- t_recieving.py
+++ t_recieving.py
@@ -6,10 +6,12 @@ target = ['192.168.68.113', 56774]
 print('host:', client.get_host())
 print('target:', target)
 client.connect(target)
+print('connected:', client.get_actual_target())
 
 @client.event(lynxy.Constants.Event.ON_MESSAGE)
 def on_message(data: lynxy.Pool.Message):
     print('recv:', data.content)
     # pyautogui.press(data.content)
 
+print(client._comm.eventRegistry)
 while True: pass
--- t_sending.py
+++ t_sending.py
@@ -6,6 +6,7 @@ target = ['192.168.68.121', 56774]
 print('host:', client.get_host())
 print('target:', target)
 client.connect(target)
+print('connected:', client.get_actual_target())
 
 while True:
     pressed = keyboard.read_key()
