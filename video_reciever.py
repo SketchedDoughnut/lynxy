562c5d5bc421d6f5f3a4cd3d1405117012ac56aa
Sketched Doughnut <sketchedbusinessemail@gmail.com>
2025-01-03 18:42:59 -0800

    updating ip v2

--- video_reciever.py
+++ video_reciever.py
@@ -6,7 +6,7 @@ print(client.get_host())
 
 out = r'D:\VScode\packages\lynxy\recieved.mp4'
 
-target = ['', 0]
+target = ['192.168.68.129', 56774]
 client.connect(target)
 
 @client.event(lynxy.Constants.Event.ON_MESSAGE)
