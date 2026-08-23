647a988fb2f0d0e25e19e903ae1ba677b2486077
Sketched Doughnut <noreply>
2026-08-23 11:56:04 -0700

    Cleaning up repository and modifying pyproject.toml

--- ignore/saves/7_31_2024-1_29am/src/lynxy/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-This is the lynxy client module. To find documentation, go to the github~!
-- Github: https://github.com/SketchedDoughnut/lynxy
-'''
-
-# extending main lynx file
-from .lynx import *
